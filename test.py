2e3ca0738cc67d8d843d6a9b1778235d284b830d
joaocosta21 <joaocostared@gmail.com>
2024-04-20 16:48:53 +0100

    add double array read

--- test.py
+++ test.py
@@ -12,5 +12,20 @@ for line in lines:
 
 # Print the values
 print(values[0])
-values[0] = values[0][0]+ "E"
-print(values[0])
+
+#double array
+input_str = "FB\tVB\tVE\nBD\tBE\tLV\nFC\tFC\tFC\n"
+
+# Split the input string by '\n' to get each line
+lines = input_str.strip().split('\n')
+
+# Initialize an empty list to store the values
+double_array = []
+
+# Iterate over each line and split it by '\t' to get the individual values
+for line in lines:
+    values = line.split('\t')
+    double_array.append(values)
+
+# Print the first line
+print(double_array[0][0])
