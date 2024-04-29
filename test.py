7162013fe301f2c3b949e120bf2de875a75d2192
joaocosta21 <joaocostared@gmail.com>
2024-04-30 00:22:00 +0100

    comments

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
-input_str = "FB\tVB\tVE\nBD\tBE\tLV\nFC\tFC\tFC\n"
+# input_str = "FB\tVB\tVE\nBD\tBE\tLV\nFC\tFC\tFC\n"
 
-# Split the input string by '\n' to get each line
-lines = input_str.strip().split('\n')
+# # Split the input string by '\n' to get each line
+# lines = input_str.strip().split('\n')
 
-# Initialize an empty list to store the values
-values = []
+# # Initialize an empty list to store the values
+# values = []
 
-# Iterate over each line and split it by '\t' to get the individual values
-for line in lines:
-    values.extend(line.split('\t'))
+# # Iterate over each line and split it by '\t' to get the individual values
+# for line in lines:
+#     values.extend(line.split('\t'))
 
-# Print the values
-print(values[0])
+# # Print the values
+# print(values[0])
 
 #double array
 input_str = "FB\tVB\tVE\nBD\tBE\tLV\nFC\tFC\tFC\n"
@@ -28,4 +28,4 @@ for line in lines:
     double_array.append(values)
 
 # Print the first line
-print(double_array[0][0])
+print(double_array)
