1b37670db306a8da17da1609375be7a986a19a05
joaocosta21 <joaocostared@gmail.com>
2024-04-30 02:35:01 +0100

    try

--- pipe.py
+++ pipe.py
@@ -109,30 +109,33 @@ class PipeMania(Problem):
         """O construtor especifica o estado inicial."""
         self.initial = PipeManiaState(board)
     
+    
     def update_pos_in(self,state: PipeManiaState):
         board: Board = state.board
         for j in range(board.dim):
             for i in range(board.dim):
                 piece = board.get_value(i,j)
                 
-                adjacent = [board.adjacent_vertical_values(i,j), board.adjacent_horizontal_values(i,j)] # 0 up 1 down 
-                
-                # if j == 0 and i == 0 and PIECE_ROTATIONS[piece]["UP"] and PIECE_ROTATIONS[piece]["LEFT"]:
-                #         print("HI")
-                # elif j == 0 and i == board.dim - 1 and PIECE_ROTATIONS[piece]["UP"] and PIECE_ROTATIONS[piece]["RIGHT"]:
-                #         print("HI")
-                # elif j == board.dim - 1 and i == 0 and PIECE_ROTATIONS[piece]["DOWN"] and PIECE_ROTATIONS[piece]["LEFT"]:
-                #         print("HI")
-                # elif j == board.dim - 1 and i == board.dim - 1 and PIECE_ROTATIONS[piece]["DOWN"] and PIECE_ROTATIONS[piece]["RIGHT"]:
-                #         print("HI")
-                # elif j == 0 and PIECE_ROTATIONS[piece]["UP"]:
-                #         print("HI")
-                # elif j == board.dim - 1 and PIECE_ROTATIONS[piece]["DOWN"]:
-                #         print("HI")
-                # elif i == 0 and PIECE_ROTATIONS[piece]["LEFT"]:
-                #         print("HI")
-                # elif i == board.dim - 1 and PIECE_ROTATIONS[piece]["RIGHT"]:
-                #         print("HI")
+                vertical_tuple = board.adjacent_vertical_values(i, j)
+                horizontal_tuple = board.adjacent_horizontal_values(i, j)
+                if j == 0 and i == 0 and PIECE_ROTATIONS[piece]["UP"] and PIECE_ROTATIONS[piece]["LEFT"]:
+                    print("HI")
+                elif j == 0 and i == board.dim - 1 and PIECE_ROTATIONS[piece]["UP"] and PIECE_ROTATIONS[piece]["RIGHT"]:
+                    print("HI")
+                elif j == board.dim - 1 and i == 0 and PIECE_ROTATIONS[piece]["DOWN"] and PIECE_ROTATIONS[piece]["LEFT"]:
+                    print("HI")
+                elif j == board.dim - 1 and i == board.dim - 1 and PIECE_ROTATIONS[piece]["DOWN"] and PIECE_ROTATIONS[piece]["RIGHT"]:
+                    print("HI")
+                elif j == 0 and PIECE_ROTATIONS[piece]["UP"]:
+                    print("HI")
+                elif j == board.dim - 1 and PIECE_ROTATIONS[piece]["DOWN"]:
+                    print("HI")
+                elif i == 0 and PIECE_ROTATIONS[piece]["LEFT"]:
+                    print("HI")
+                elif i == board.dim - 1 and PIECE_ROTATIONS[piece]["RIGHT"]:
+                    print("HI")
+                elif vertical_tuple[0] not in PIECE_ROTATIONS[piece]["UP"]:
+                    print("HI")
         board.print()
         
     def actions(self, state: PipeManiaState):
