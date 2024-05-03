e5c4b0dae8bab8b7e2c346ee2807bf6f3d45cf72
joaocosta21 <joaocostared@gmail.com>
2024-05-04 00:13:06 +0100

    waiting

--- test.py
+++ test.py
@@ -29,3 +29,32 @@ for line in lines:
 
 # Print the first line
 print(double_array)
+
+
+# def update_pos_in(self,state: PipeManiaState):
+    #     board: Board = state.board
+        # for j in range(board.dim):
+        #     for i in range(board.dim):
+        #         piece = board.get_value(i,j)
+                
+        #         vertical_tuple = board.adjacent_vertical_values(i, j)
+        #         horizontal_tuple = board.adjacent_horizontal_values(i, j)
+        #         if j == 0 and i == 0 and PIECE_ROTATIONS[piece]["UP"] and PIECE_ROTATIONS[piece]["LEFT"]:
+        #             print("HI")
+        #         elif j == 0 and i == board.dim - 1 and PIECE_ROTATIONS[piece]["UP"] and PIECE_ROTATIONS[piece]["RIGHT"]:
+        #             print("HI")
+        #         elif j == board.dim - 1 and i == 0 and PIECE_ROTATIONS[piece]["DOWN"] and PIECE_ROTATIONS[piece]["LEFT"]:
+        #             print("HI")
+        #         elif j == board.dim - 1 and i == board.dim - 1 and PIECE_ROTATIONS[piece]["DOWN"] and PIECE_ROTATIONS[piece]["RIGHT"]:
+        #             print("HI")
+        #         elif j == 0 and PIECE_ROTATIONS[piece]["UP"]:
+        #             print("HI")
+        #         elif j == board.dim - 1 and PIECE_ROTATIONS[piece]["DOWN"]:
+        #             print("HI")
+        #         elif i == 0 and PIECE_ROTATIONS[piece]["LEFT"]:
+        #             print("HI")
+        #         elif i == board.dim - 1 and PIECE_ROTATIONS[piece]["RIGHT"]:
+        #             print("HI")
+        #         elif vertical_tuple[0] not in PIECE_ROTATIONS[piece]["UP"]:
+        #             print("HI")
+        # board.print()
